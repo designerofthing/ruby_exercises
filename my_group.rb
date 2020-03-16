f96902a17280c648c06cdbd958d65224302fd8d2
Steve <designerofthings@gmail.com>
2020-03-16 10:43:28 +0100

    Initial commit

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+my_group = []
+person_1 = {name: "Thomas", gender: "male", age: 48}
+person_2 = {name: "Noel", gender: "male", age:29}
+person_3 = {name: "Faraz", gender: "male", age:36}
+my_group.push person_1
+my_group.push person_2
+my_group.push person_3
+
+puts my_group
